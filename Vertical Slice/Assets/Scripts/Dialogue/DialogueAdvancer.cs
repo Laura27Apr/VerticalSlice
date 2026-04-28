41ece4b92e62f6cb8af30c610f7bedeac8735986
Laura27Apr <leyangl4@uci.edu>
2026-04-27 22:53:40 -0700

    Change Dialogue System into C# Code
    Since it does not work in Visual Scripting and I have spent too much time on it

--- Vertical Slice/Assets/Scripts/Dialogue/DialogueAdvancer.cs
+++ Vertical Slice/Assets/Scripts/Dialogue/DialogueAdvancer.cs
@@ -1,23 +1,56 @@
 ﻿using UnityEngine;
 using Unity.VisualScripting;
+using TMPro;
 
 public class DialogueAdvancer : MonoBehaviour
 {
-    [SerializeField] private DialogueNode nextLine;
+    public static DialogueAdvancer _Instance { get; private set; }
 
-    public void Start()
+    private void Awake()
     {
-        EventBus.Trigger(EventNames.NewDialogueEvent, nextLine);
+        if (_Instance != null && _Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        _Instance = this;
     }
 
-    //Button hooks up to this method
-    //public void ChooseDialogue()
-    //{
-       // EventBus.Trigger(EventNames.NewDialogueEvent, nextLine);
-    //}
+    [SerializeField] private DialogueNode startLine;
+    [SerializeField] private TMP_Text dialogueText;
 
-    public void PrintHello()
+    private DialogueNode currentNode;
+    private int currentLineIndex = 0;
+
+    public void StartDialogue()
     {
-        Debug.Log("hello!");
+        Debug.Log("Start Dialogue");
+
+        currentNode = startLine;
+        currentLineIndex = 0;
+
+        ShowCurrentLine();
     }
+    public void ShowCurrentLine()
+    {
+        if (currentNode == null) return;
+
+        if (currentLineIndex < currentNode.Lines.Length)
+        {
+            dialogueText.text = currentNode.Lines[currentLineIndex];
+
+            currentLineIndex++;
+        }
+
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShowCurrentLine();
+        }
+    }
+
 }
