9c4dd25fa3404074493fbb463ae11e325807a230
Laura27Apr <leyangl4@uci.edu>
2026-05-05 22:02:58 -0700

    Add Reading Material

---
+++ Vertical Slice/Assets/Scripts/BookInteract.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class BookInteract : MonoBehaviour
+{
+    [SerializeField] private GameObject promptUI;
+    [SerializeField] private GameObject readUI;
+    [SerializeField] private Transform player;
+    [SerializeField] private float interactDistance = 2f;
+
+    private bool playerInteract;
+
+    public void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        playerInteract = distance <= interactDistance;
+
+        promptUI.SetActive(playerInteract && !readUI.activeSelf);
+
+        if (playerInteract && Input.GetKeyDown(KeyCode.F))
+        {
+            readUI.SetActive(true);
+            promptUI.SetActive(false);
+            Debug.Log("F pressed");
+        }
+
+        if (readUI.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            readUI.SetActive(false);
+
+            if (playerInteract)
+            {
+                promptUI.SetActive(true);
+            }
+
+        }
+    }
+
+   
+}
---
+++ Vertical Slice/Assets/Scripts/PromptUIFacing.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PromptUIFacing : MonoBehaviour
+{
+    public void LateUpdate()
+    {
+        if (Camera.main == null) return;
+
+        Vector3 direction = Camera.main.transform.position - transform.position;
+
+        direction.y = 0;
+        
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+}
