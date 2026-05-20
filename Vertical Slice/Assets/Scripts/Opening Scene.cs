a521ed2cce82dfbb58b5edbd811d5b72b358959b
Laura27Apr <leyangl4@uci.edu>
2026-05-19 23:06:27 -0700

    Add more interactable items & Timeline Transition

---
+++ Vertical Slice/Assets/Scripts/Opening Scene.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+
+public class OpeningScene : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName = "GameScene";
+    [SerializeField] private PlayableDirector fadeOutTimeline;
+
+    private void Start()
+    {
+        fadeOutTimeline.stopped += OnFadeOutFinished;
+    }
+
+    public void ContinueGame()
+    {
+        fadeOutTimeline.Play();
+    }
+
+    private void OnFadeOutFinished(PlayableDirector director)
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    private void OnDestroy()
+    {
+        fadeOutTimeline.stopped -= OnFadeOutFinished;
+    }
+}
---
+++ Vertical Slice/Assets/Scripts/OtherReadableInteract.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Unity.VisualScripting;
+
+public class OtherReadableInteract : MonoBehaviour
+{
+    [SerializeField] private GameObject promptUI;
+    [SerializeField] private GameObject readUI;
+    [SerializeField] private Transform player;
+    [SerializeField] private float interactDistance = 3f;
+
+    private bool playerInRange;
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+        playerInRange = distance <= interactDistance;
+
+        promptUI.SetActive(playerInRange && !readUI.activeSelf);
+
+        if (playerInRange && Input.GetKeyDown(KeyCode.F))
+        {
+            readUI.SetActive(true);
+            promptUI.SetActive(false);
+
+            Variables.ActiveScene.Set("isReading", true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        if (readUI.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            readUI.SetActive(false);
+
+            Variables.ActiveScene.Set("isReading", false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
